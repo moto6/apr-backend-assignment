207fe379bd9640ea4a8befe122acbbd9bd8730f0
moto6 <ultraraptor@naver.com>
2026-03-14 11:43:59 +0900

    feat : first commit

---
+++ src/test/java/com/apr/AprBackendAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprBackendAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
