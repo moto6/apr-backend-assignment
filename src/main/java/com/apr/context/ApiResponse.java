02e8caea628d0f1b63f1d29d2dc2dc339d2b03cd
moto6 <ultraraptor@naver.com>
2026-03-14 13:58:42 +0900

    feat : http 계층 공통 응답 구조 생성

---
+++ src/main/java/com/apr/context/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.apr.context;
+
+public record ApiResponse<T>(
+        T data
+) {
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(data);
+    }
+
+    public static ApiResponse<Void> ok() {
+        return new ApiResponse<>(null);
+    }
+}
