1c7fe91d89e5d75a452b15c5d7b685259e35ca47
moto6 <ultraraptor@naver.com>
2026-03-16 22:22:43 +0900

    chore : 불필요한 주석 제거

--- src/main/java/com/apr/config/H2ServerConfig.java
+++ src/main/java/com/apr/config/H2ServerConfig.java
@@ -8,12 +8,10 @@ import org.springframework.context.annotation.Profile;
 import java.sql.SQLException;
 
 @Configuration
-@Profile("local") // 로컬 개발/테스트 시에만 작동하도록 설정
+@Profile("local")
 public class H2ServerConfig {
-
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2TcpServer() throws SQLException {
-        // 9092 포트로 TCP 서버를 띄워 외부 접속을 허용합니다.
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
